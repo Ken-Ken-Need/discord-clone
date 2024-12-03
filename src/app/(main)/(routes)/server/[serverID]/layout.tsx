fc981929e81a2931936d7f57548903683484580e
Ken <kg3209@nyu.edu>
2024-12-02 19:06:15 -0500

    Set up of channel bar and channel items

--- src/app/(main)/(routes)/server/[serverID]/layout.tsx
+++ src/app/(main)/(routes)/server/[serverID]/layout.tsx
@@ -1,8 +1,18 @@
-const MainLayout = ({ children }: { children: React.ReactNode }) => {
+import { ChannelBar } from "@/components/channels/channel-bar";
+import getServerFromID from "@/lib/getServerFromID";
+import { ChannelType } from "@/database/typesOfSchemas";
+const MainLayout = async ({ children, params }: { children: React.ReactNode, params: { serverID: string } }) => {
+    const s = await getServerFromID(params.serverID);
+    console.log("channels:", s.channels)
     return (
         <div className="h-full" >
             <div className="h-full w-[300px] bg-indigo-950 text-zinc-50 z-30 flex-col fixed inset-y-0">
-                channels
+                <ChannelBar />
+                {s.channels.map((channel: ChannelType) => {
+                    return (
+                        <div key={String(channel._id)}>{channel.name}</div>
+                    )
+                })}
             </div>
             <main className="pl-[300px]">
                 {children}
---
+++ src/components/channels/channel-bar.tsx
@@ -0,0 +1,8 @@
+"use client";
+export const ChannelBar = () => {
+    return (
+        <div>
+            Hello world
+        </div>
+    )
+}
