dc21de236443934c5da8d771907714a63a07b88e
Ken <kg3209@nyu.edu>
2024-12-03 16:54:06 -0500

    Channel pages created

--- src/app/(main)/(routes)/server/[serverID]/layout.tsx
+++ src/app/(main)/(routes)/server/[serverID]/layout.tsx
@@ -1,6 +1,8 @@
 import { ChannelBar } from "@/components/channels/channel-bar";
 import getServerFromID from "@/lib/getServerFromID";
 import { ChannelType } from "@/database/typesOfSchemas";
+import { ChannelSwitch } from "@/components/channels/channel-switch";
+import { Separator } from "@/components/ui/separator";
 const MainLayout = async ({ children, params }: { children: React.ReactNode, params: { serverID: string } }) => {
     const { serverID } = await params;
     const s = await getServerFromID(serverID);
@@ -9,9 +11,10 @@ const MainLayout = async ({ children, params }: { children: React.ReactNode, par
         <div className="h-full" >
             <div className="h-full w-[300px] bg-slate-900 text-zinc-50 z-30 flex-col fixed inset-y-0">
                 <ChannelBar />
+                <Separator className="bg-zinc-700 rounded-full" />
                 {s.channels.map((channel: ChannelType) => {
                     return (
-                        <div key={String(channel._id)}>{channel.name}</div>
+                        <ChannelSwitch key={String(channel._id)} channelId={String(channel._id)} serverID={serverID} channelName={channel.name} />
                     )
                 })}
             </div>
--- src/components/channels/channel-bar.tsx
+++ src/components/channels/channel-bar.tsx
@@ -1,13 +1,9 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
 import { useModal } from "@/hooks/user-modal-store";
 export const ChannelBar = () => {
     const { onOpen } = useModal();
     return (
-        <div>
-            <Button onClick={() => onOpen("createChannel")} className="w-3/4 m-5 justify-center bg-indigo-800 "> Create Channel</Button>
-            <Separator className="bg-zinc-700 rounded-full" />
-        </div>
+        <Button onClick={() => onOpen("createChannel")} className="w-3/4 m-5  bg-indigo-800 "> Create Channel</Button>
     )
 }
